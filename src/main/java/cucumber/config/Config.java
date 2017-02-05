56ec0e0720ca895938bdc073b1283061633e812b
Steve Momcilovic <smomcilovic@tapqa.com>
2017-02-05 17:22:06 -0600

    updated with appium, example almost complete

---
+++ src/main/java/cucumber/config/Config.java
@@ -0,0 +1,12 @@
+package cucumber.config;
+
+
+public class Config {
+
+    public final String url;
+
+    public Config() {
+        this.url = "http://127.0.0.1:4723/wd/hub";
+
+    }
+}
--- src/test/java/cucumber/support/Hooks.java
+++ src/test/java/cucumber/support/Hooks.java
@@ -3,14 +3,33 @@ package cucumber.support;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import cucumber.config.Config;
+
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
     private WebDriver driver;
+    private String nodejsPath;
+    private String appiumMainPath;
+    private AppiumDriverLocalService service;
+    private AppiumDriver<WebElement> appiumDriver;
 
     public WebDriver getDriver() {
         return driver;
@@ -20,7 +39,39 @@ public class Hooks {
     @Before
     public void beforeScenario(Scenario scenario) {
 
-        driver = new ChromeDriver();
+        Config config = new Config();
+
+        try {
+            System.setProperty(AppiumServiceBuilder.NODE_PATH,
+            "usr/local/bin");
+
+            System.setProperty(AppiumServiceBuilder.APPIUM_PATH,
+            "the path to the desired appium.js or main.js");
+
+            URL url = new URL(config.url);
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Browser");
+            caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+            caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 120);
+            driver = new AndroidDriver<WebElement>(url, caps);
+
+        } catch (MalformedURLException ex) {
+
+            throw new RuntimeException(ex);
+
+        }
+
+
+        // TODO: Set paths
+
+
+//        TODO: build and start appium server
+//        AppiumDriverLocalService service = AppiumDriverLocalService.buildDefaultService();
+//        service.start();
+//        service.stop();
+
+        // TODO: build out driver select method
+//        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
